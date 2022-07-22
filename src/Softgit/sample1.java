5b4e5bfdf3a4be1f9a89eadfb0929ba98019896d
Lenovo <Lenovo@Lenovo-pc>
2022-07-22 19:45:17 +0530

    [Maya]: Added sample 1 class

---
+++ src/Softgit/sample1.java
@@ -0,0 +1,16 @@
+package Softgit;
+
+public class sample1 
+{
+
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		a=10;
+		b=10;
+		c=a+b;
+		System.out.println("addition:- "+c);
+
+	}
+
+}
